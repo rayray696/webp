8e2658161900ab540f2ca27c7df336fc0c1185e0
rayray696 <100269283+rayray696@users.noreply.github.com>
2022-05-15 18:46:34 +0800

    CGU Login

--- hw7/login.js
+++ hw7/login.js
@@ -1,13 +1,22 @@
+const styleArgument3={width:'150px'};
+const styleArgument2={ paddingTop: '10px',
+                       paddingLeft: '10px',
+                       paddingRight:'10px',
+                      };
+const styleArgument1={ paddingTop: '10px',
+                       paddingLeft: '10px',
+                       paddingRight:'10px',
+                       };
 const Login=()=>{
-        return (
+  return (
           <div>
              <form>
                <label>CGU Login</label><br/>
                 <label>Username</label>
-                <input type="text" placeholder="Enter Username" required/><br/>
+                <input type="text"   style={styleArgument1} placeholder="Enter Username" required/><br/><br/>
                 <label>Password</label>
-                <input type="password" placeholder="Enter Password" required/><br/>
-                <button type="submit">Login</button>
+                <input type="password"  style={styleArgument2} placeholder="Enter Password" required/><br/><br/>
+                <button type="submit" style={styleArgument3}>Login</button>
              </form>
           </div>
         );  
